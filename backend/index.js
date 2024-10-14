f8c5fca8ec574ca76a92985caf63307afb9602ec
Manmath Mungde <123142938+manmath18@users.noreply.github.com>
2024-10-14 16:23:56 +0530

    Added

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use(cookieParser());
 const corsOptions = {
     origin: `${process.env.FRONTEND_URL}`,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
 app.use(cors(corsOptions));
